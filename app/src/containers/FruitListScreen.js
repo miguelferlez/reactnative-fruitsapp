77b983b2fc5922b1a397495dea53b1442461ea84
AzureAD\DanielValienteMuñoz <daniel.ma42585@cesurformacion.com>
2022-01-17 13:47:21 +0100

    v1.01

--- app/src/containers/FruitListScreen.js
+++ app/src/containers/FruitListScreen.js
@@ -1,10 +1,56 @@
-import React from "react";
-import { View, Text } from "react-native";
-function fruitListScreen() {
-    return(
+import React, { useState, useEffect } from "react";
+import { View, Text, StyleSheet,FlatList,Image } from "react-native";
+ 
+function fruitListScreen({route}) {
+   const [fruits,setFruits] = useState(null);
+   
+   useEffect (() => {
+    fetch ("http://10.0.2.2:8080/fruits")
+    .then (response => response.json())
+    .then((responseJson) =>{
+        console.log('getting data from fetch',responseJson);
+        setFruits(responseJson);
+    })
+    .catch(error => console.log(error));
+},[])
+
+
+const renderItem = ({item}) => (
+ 
+    <View >
+       
+        <View style={{ marginLeft:'10%'}}>
+       
+               <Image
+                        style={{width: 50, height:50}}
+                        source={require('../assets/manzana.jpg')}/>
+            <Text style={{fontWeight:"bold"}}> Nombre: <Text>{item.name} </Text></Text>
+            </View>
+
+            <View style={{ marginLeft:'75%'}}>
+            <Text >Precio:{item.price} </Text>
+            </View>
+    </View>
+)
+   
+   return(
         <View>
-            <Text>En esta ventana puedes ver la lista de frutas</Text>
+           <FlatList
+        data={fruits}
+        renderItem={renderItem}
+        keyExtractor={item =>item.id}
+        />
         </View>
     );
 }
+
+
+const style = StyleSheet.create({
+    contenedor:{
+        flex: 1,
+        alignItems: 'center',
+        backgroundColor: "#EDC669",
+       
+    }
+})
 export default fruitListScreen;
