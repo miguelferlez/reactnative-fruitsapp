58e6dc36f23af5fda078e128d8f41ebbb328d67b
Miguel <miguelferlez@gmail.com>
2022-01-17 13:27:23 +0100

    body y contenido innecesario corregido

--- app/App.js
+++ app/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { View } from 'react-native';
 import BottomTabNavigator from './src/navigation/BottomTab';
 import { NavigationContainer } from '@react-navigation/native';
 function App() {
--- app/src/styles/Body.js
+++ app/src/styles/Body.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { StyleSheet } from "react-native";
-function body () {
+const body = StyleSheet.create({
     
-}
+});
 export default body;
